28a5d624658f36733b98730beac91ddcc73caa7e
Idar Ulvestad Haugstuen <idaruh@gmail.com>
2021-12-30 09:59:35 +0100

    Fix

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 if __name__ == '__main__':
     print("Starting raspi-cam-to-s3bucket integration...")
-    image_files_folder = getenv("RASPI_CAM_ROOT_FOLDER") + "/files"
+    image_files_folder = getenv("RASPI_CAM_ROOT_FOLDER") + "/files/"
     camera = Camera((2592, 1944), image_files_folder)
     s3_uploader = S3Uploader(image_files_folder)
     image_scheduler = ImageScheduler(camera, s3_uploader)
